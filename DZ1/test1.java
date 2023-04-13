9c81ee495380f7afbb4c5a5a85e27fa990c27c2a
Lidiya <lesnikoval@yandex.ru>
2023-04-13 23:02:48 +0300

    Домашнее задание 1

--- DZ1/test1.java
+++ DZ1/test1.java
@@ -1,5 +1,6 @@
 public class test1 {
     public static void main(String[] args) {
+        //Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
         
     }
 }
---
+++ DZ1/test2.java
@@ -0,0 +1,6 @@
+public class test2 {
+    public static void main(String[] args) {
+        //Вывести все простые числа от 1 до 1000
+        
+    }
+}

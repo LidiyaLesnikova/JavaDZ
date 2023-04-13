c9986186360db6571be737f2d8f4a70ea7fffa11
Lidiya <lesnikoval@yandex.ru>
2023-04-13 23:29:24 +0300

    Задание 1

---
+++ DZ1/task1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        //Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number n ");
+        int n = scanner.nextInt();
+        System.out.println(calculateProductSum(n));
+        scanner.close();
+    }
+
+    static String calculateProductSum(int n) {
+        int prodact = 1;
+        int sum = 0; 
+        for (int i = 1; i <= n; i++) {
+            prodact*=i;
+            sum+=i;
+        }
+        return String.format("%d-ое треугольного числа = %d, %d! = %d \n", n, sum, n, prodact);
+    }
+}

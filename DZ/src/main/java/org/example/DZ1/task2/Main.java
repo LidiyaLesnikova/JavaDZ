ebe87aa838367a0ecf93a908841c39223bf2e93d
Lidiya <lesnikoval@yandex.ru>
2023-04-17 23:21:47 +0300

    Домашнее задание 1

--- DZ/src/main/java/org/example/DZ1/task2/Main.java
+++ DZ/src/main/java/org/example/DZ1/task2/Main.java
@@ -2,8 +2,6 @@ package org.example.DZ1.task2;
 
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
        /* Дана последовательность целых чисел, оканчивающаяся нулем. Найти сумму положительных

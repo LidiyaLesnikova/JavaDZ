bd9d0d9c3f1306d28ada3f13232a4e8884bb0199
Lidiya <lesnikoval@yandex.ru>
2023-04-19 11:16:07 +0300

    Задача 3

---
+++ DZ/src/main/java/org/example/DZ2/task3/Main.java
@@ -0,0 +1,31 @@
+package org.example.DZ2.task3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// Дан массив целых чисел. Заменить отрицательные элементы на сумму индексов двузначных элементов массива
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = {1, 20, 4, -3, 16, 5, -3, 0, -4, 23};
+        System.out.println(replaceArr(array));
+    }
+
+    static String replaceArr(int[] arr) {
+        String list = Arrays.toString(arr);
+        System.out.println(list);
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]>9 && arr[i]<100) {
+                sum+=i;
+            }
+            if (arr[i]<0) {
+                list = list.replace(String.valueOf(arr[i]),"XX");
+            }
+        }
+        list = list.replaceAll("XX",String.valueOf(sum));
+        return list;
+    }
+}

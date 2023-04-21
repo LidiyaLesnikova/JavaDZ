535c2d29cbca25bdb71510874f0871d079497cce
Lidiya <lesnikoval@yandex.ru>
2023-04-21 13:18:05 +0300

    Задача 2

--- DZ/src/main/java/org/example/DZ3/task2/Main.java
+++ DZ/src/main/java/org/example/DZ3/task2/Main.java
@@ -1,9 +1,33 @@
 package org.example.DZ3.task2;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 //Сведения о товаре состоят из наименования, страны-производителя, веса, цены, сорта.
 // Получить наименования товаров заданного сорта с наименьшей ценой.
 public class Main {
     public static void main(String[] args) {
+        List<Product> productList = getProduct();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input necessary variety product: ");
+        int variety = sc.nextInt();
+        Double minPrice = productList.stream().filter(pr -> pr.variety == variety).min(Comparator.comparingDouble(Product::getPrice)).get().price;
+        productList = productList.stream().filter(pr -> pr.variety == variety && pr.price.equals(minPrice)).toList();
+        System.out.println("Product "+variety+" variety with min price: \n"+productList);
+    }
 
+    public static List<Product> getProduct() {
+        ArrayList<Product> productList = new ArrayList<Product>();
+        productList.add(new Product("product 1","country 1", 600.0, 50.5, 1));
+        productList.add(new Product("product 2","country 1", 100.0, 100.0, 1));
+        productList.add(new Product("product 3","country 2", 500.0, 50.0, 2));
+        productList.add(new Product("product 4","country 2", 300.0, 30.0, 1));
+        productList.add(new Product("product 5","country 3", 90.0, 20.0, 3));
+        productList.add(new Product("product 6","country 3", 10.0, 30.0, 1));
+        productList.add(new Product("product 7","country 4", 1000.0, 150.0, 2));
+        return productList;
     }
 }
 
--- DZ/src/main/java/org/example/DZ3/task2/Product.java
+++ DZ/src/main/java/org/example/DZ3/task2/Product.java
@@ -3,18 +3,17 @@ package org.example.DZ3.task2;
 public class Product {
     String nameProduct;
     String country;
-    Float weight;
-    Float price;
+    Double weight;
+    Double price;
     Integer variety;
 
-    public Product(String nameProduct, String country, Float weight, Float price, Integer variety) {
+    public Product(String nameProduct, String country, Double weight, Double price, Integer variety) {
         this.nameProduct = nameProduct;
         this.country = country;
         this.weight = weight;
         this.price = price;
         this.variety = variety;
     }
-
 
     public Double getPrice() {
         return price;
@@ -24,6 +23,6 @@ public class Product {
     public String toString() {
         return "Наименование: '" + nameProduct + '\'' +
                 ", Цена: " + price +
-                ", Сорт: " + variety;
+                ", Сорт: " + variety + "\n";
     }
 }

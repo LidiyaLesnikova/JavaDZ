18cdf805743b26b8be97ceec7c7c28fbafc33d6c
Lidiya <lesnikoval@yandex.ru>
2023-04-25 22:05:31 +0300

    Задача 2

--- DZ/src/main/java/org/example/DZ4/task2/Main.java
+++ DZ/src/main/java/org/example/DZ4/task2/Main.java
@@ -1,7 +1,9 @@
 package org.example.DZ4.task2;
 
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
 
 //Дана строку, s содержащая только символы '(', ')', '{', '}''[', ']', определите, является ли введенная строка корректной.
 //
@@ -11,28 +13,26 @@ import java.util.Scanner;
 // каждой закрывающей скобке соответствует открывающая скобка того же типа.
 public class Main {
     public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Введите длину списка: ");
-            int n = sc.nextInt();
-            LinkedList<Integer> list = getList(n);
-            System.out.println(list);
-            System.out.println(reservList(list));
-        }
+        System.out.println("'()' - "+isValid("()"));
+        System.out.println("'()[]{}' - "+isValid("()[]{}"));
+        System.out.println("'(]' - "+isValid("(]"));
+        System.out.println("'([]{})' - "+isValid("([]{})"));
+        System.out.println("'([]{)}' - "+isValid("([]{)}"));
+    }
 
-        static LinkedList<Integer> getList(int n) {
-            LinkedList<Integer> list = new LinkedList<Integer>();
-            for (int i = 1; i < n; i++) {
-                list.add(i);
+    static boolean isValid(String s) {
+        List<String> standart = Arrays.asList("()" , "[]" , "{}");
+        List<String> list = new ArrayList<>(List.of(s.split("")));
+        int i = 0;
+        while (i < list.size()-1) {
+            if (standart.contains(list.get(i)+list.get(i+1))) {
+                list.remove(i);
+                list.remove(i);
+                i = 0;
+            } else {
+                i++;
             }
-            return list;
         }
-
-        static LinkedList<Integer> reservList(LinkedList<Integer> list) {
-            int a = 0;
-            for (int i = 0; i < list.size(); i++) {
-                a = list.pollLast();
-                list.add(i, a);
-            }
-            return list;
-        }
+        return (list.size()==0) ? true : false;
+    }
 }

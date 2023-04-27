427e3ff146d527c7c42a18113e378e0ac947dbf6
Lidiya <lesnikoval@yandex.ru>
2023-04-28 00:54:07 +0300

    Задача 2

--- DZ/src/main/java/org/example/DZ5/task2/Main.java
+++ DZ/src/main/java/org/example/DZ5/task2/Main.java
@@ -1,7 +1,12 @@
 package org.example.DZ5.task2;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.LinkedHashMap;
 
 /*Пусть дан список сотрудников:
 Иван Иванов

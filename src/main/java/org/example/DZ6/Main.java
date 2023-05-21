0864967bc03878255d291cca9532cfd801fd917a
Lidiya <lesnikoval@yandex.ru>
2023-05-21 23:52:02 +0300

    Домашнее задание 6

--- src/main/java/org/example/DZ6/Main.java
+++ src/main/java/org/example/DZ6/Main.java
@@ -13,44 +13,112 @@ package org.example.DZ6;
 также в Map.
 • Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.*/
 
+import java.lang.reflect.Field;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         List<Notebook> notebookList = getNotebooks();
-        System.out.println(notebookList);
-//        Integer maxSimpleNumber = bookList.stream().max(Comparator.comparingInt(Book::getNumberPages)).get().numberPages;
-//        List<Integer> simpleNumbers = getSimpleNumbers(maxSimpleNumber);
-//        bookList = bookList.stream().filter(book -> book.author.contains("A") && book.yearPublication>=2010 &&
-//        simpleNumbers.contains(book.numberPages)).toList();
-//        System.out.println(bookList);
+        Map<Integer, Object> filter = selectUser(notebookList);
+        System.out.println((filter.isEmpty()) ? "Фильтр пользотелем не задан":filterNotebook(notebookList,filter));
     }
 
+    /**
+     * Создание множества ноутбуков
+     * @return список ноутбуков
+     */
     public static List<Notebook> getNotebooks() {
             ArrayList<Notebook> notebookList = new ArrayList<Notebook>();
-            notebookList.add(new Notebook("Lenovo", 512, 8, "Windows", "grey", "China"));
-            notebookList.add(new Notebook("Lenovo", 512, 8, "Windows", "grey", "China");
-            notebookList.add(new Notebook("Lenovo", 512, 8, "Windows", "grey", "China"));
-
+            notebookList.add(new Notebook("Lenovo","SSD",512, 8, "Windows 10", "grey", "China", 20000.00));
+            notebookList.add(new Notebook("Lenovo","HD",218, 4, "Windows 8", "black", "China", 15000.00));
+            notebookList.add(new Notebook("iRU","HD",1000, 8, "Windows 10", "grey", "Russia", 30000.00));
+            notebookList.add(new Notebook("Acer","SSD" ,512, 8, "Windows 10", "grey", "China", 30000.00));
+            notebookList.add(new Notebook("Acer","HD" ,218, 4, "Windows 10", "grey", "China", 19000.00));
+            notebookList.add(new Notebook("Echips","SSD" ,512, 8, "Windows 11", "grey", "China", 18000.00));
+            notebookList.add(new Notebook("Makbook","SSD" ,512, 16, "macOS", "white", "China", 80000.00));
             return notebookList;
     }
 
-//    String brandNotebook;
-//    Integer volumeHD;
-//    Integer volumeRAM;
-//    String nameOS;
-//    String colorProduct ;
-//    String countryManufacture;
+    /**
+     * метод запрашивает у пользователя критерии отбора ноутбуков из имеющихся в базе и возвращает полученный фильтр
+     * @param notebookList список всех ноутбуков
+     * @return отбор (фильтр), определенный пользователем.
+     */
+    public static Map<Integer, Object> selectUser(List<Notebook> notebookList) {
+        Map<Integer, Object> filter = new HashMap<>();
+        Scanner scan = new Scanner(System.in);
+        int n = 0;
+        do {
+            System.out.println("\n");
+            Field[] fields = Notebook.class.getDeclaredFields();
+            for(int i = 0; i < fields.length; i++) {
+                System.out.printf("%d - %s\n",i+1,fields[i].getName());
+            }
+            System.out.println(
+                    "9 - вывод всего списка без отбора\n" +
+                    "0 - ВЫХОД\n" +
+                    "Введите цифру, соответствующую необходимому критерию отбора: ");
+            n = scan.nextInt();
+            scan.nextLine();
+            if (n>=1 && n<=8) {
+                Set<Object> list = new HashSet<Object>();
+                for (Notebook notebook : notebookList) {
+                    list.add(notebook.getField(n));
+                }
+                System.out.println(list);
 
-//    static List<Integer> getSimpleNumbers(int n) {
-//            ArrayList<Integer> simpleNumbers = new ArrayList<>();
-//            for (int i = 2; i <= n; i++) {
-//            int count = 0;
-//            for (int j = 2; j <= i; j++) {
-//            if (i%j==0) count++;
-//            }
-//            if (count == 1) {
-//            simpleNumbers.add(i);
-//            }
-//            }
-//            return simpleNumbers;
-//    }
+                if (list.iterator().next() instanceof String ) {
+                    System.out.println("введите отбор из приведенных выше: ");
+                    String rez = scan.nextLine().toLowerCase();
+                    if (list.toString().toLowerCase().contains(rez))
+                        filter.put(n, rez);
+                    else
+                        System.out.println("Таких параметров нет");
+                } else if (list.iterator().next() instanceof Integer) {
+                    System.out.println("введите отбор (минимум из приведенных выше)): ");
+                    Integer rez = scan.nextInt();
+                    filter.put(n, rez);
+                } else {
+                    System.out.println("введите диапазон цены от: ");
+                    List<Double> price = new ArrayList<>(2);
+                    price.add(scan.nextDouble());
+                    System.out.println("введите диапазон цены до: ");
+                    price.add(scan.nextDouble());
+                    filter.put(n, price);
+                }
+            }
+            else if (n==9) {
+                System.out.println(notebookList);
+            }
+            else {
+                if (n!=0) System.out.println("Неверно выбранная цифра, попробуйте еще раз");
+            }
+        } while (n!=0);
+        scan.close();
+        System.out.println("Отбор: "+filter);
+        return filter;
+    }
+
+    /**
+     *
+     * @param notebookList список всех ноутбуков
+     * @param filter отбор (фильтр), определенный пользователем
+     * @return отфильтрованный список ноутбуков
+     */
+    public static List<Notebook> filterNotebook(List<Notebook> notebookList, Map<Integer, Object> filter) {
+        for (int i = 1; i <= Notebook.class.getDeclaredFields().length; i++) {
+            if (filter.containsKey(i)) {
+                Integer temp = i;
+                if (filter.get(i) instanceof String) {
+                    notebookList = notebookList.stream().filter(nb -> nb.getField(temp).toString().toLowerCase().contains((String)filter.get(temp))).toList();
+                } else if (filter.get(i) instanceof Integer) {
+                    notebookList = notebookList.stream().filter(nb -> (Integer) nb.getField(temp) >= (Integer) filter.get(temp)).toList();
+                } else {
+                    notebookList = notebookList.stream().filter(nb -> ((Double) nb.getField(temp) >= ((ArrayList<Double>)filter.get(temp)).get(0)) &&
+                            ((Double) nb.getField(temp) <= ((ArrayList<Double>)filter.get(temp)).get(1))).toList();
+                }
+            }
+        }
+        return notebookList;
+    }
 }
--- src/main/java/org/example/DZ6/Notebook.java
+++ src/main/java/org/example/DZ6/Notebook.java
@@ -1,42 +1,68 @@
 package org.example.DZ6;
 
+import java.lang.reflect.Type;
+
 public class Notebook {
     String brandNotebook;
+    String typeHD;
     Integer volumeHD;
     Integer volumeRAM;
     String nameOS;
     String colorProduct ;
     String countryManufacture;
+    Double price;
 
-    public Notebook(String brandNotebook, Integer volumeHD, Integer volumeRAM, String nameOS, String colorProduct, String countryManufacture) {
+    public Notebook(String brandNotebook, String typeHD, Integer volumeHD, Integer volumeRAM, String nameOS,
+                    String colorProduct, String countryManufacture, Double price) {
         this.brandNotebook = brandNotebook;
+        this.typeHD = typeHD;
         this.volumeHD = volumeHD;
         this.volumeRAM = volumeRAM;
         this.nameOS = nameOS;
         this.colorProduct = colorProduct;
         this.countryManufacture = countryManufacture;
+        this.price = price;
     }
 
-//    @Override
-//    public String toString() {
-//        return  "nameBook='" + nameBook + '\'' +
-//                ", author='" + author + '\'' +
-//                ", price=" + price +
-//                ", yearPublication=" + yearPublication +
-//                ", numberPages=" + numberPages + "\n";
-//    }
-    @java.lang.Override
-    public java.lang.String toString() {
-        return "Notebook{" +
-                "brandNotebook='" + brandNotebook + '\'' +
-                ", volumeHD=" + volumeHD +
-                ", volumeRAM=" + volumeRAM +
-                ", nameOS='" + nameOS + '\'' +
-                ", colorProduct='" + colorProduct + '\'' +
-                ", countryManufacture='" + countryManufacture + '\'' +
-                '}';
+    @Override
+    public String toString() {
+        return  '\n'+"Бренд = '" + brandNotebook + '\'' +
+                ", жесткий диск = " + typeHD +' '+ volumeHD +"Mb"+
+                ", размер RAM = " + volumeRAM +
+                ", OS = " + nameOS +
+                ", цвет = " + colorProduct +
+                ", страна производства = " + countryManufacture +
+                ", цена = " + price +"руб.";
+    }
 
-    //public Integer getNumberPages() {
-        return numberPages;
+    public Object getField(Integer num) {
+        Object rez = null;
+        switch (num) {
+            case 1:
+                rez = brandNotebook;
+                break;
+            case 2:
+                rez = typeHD;
+                break;
+            case 3:
+                rez = volumeHD;
+                break;
+            case 4:
+                rez = volumeRAM;
+                break;
+            case 5:
+                rez = nameOS;
+                break;
+            case 6:
+                rez = colorProduct;
+                break;
+            case 7:
+                rez = countryManufacture;
+                break;
+            case 8:
+                rez = price;
+                break;
+        }
+        return rez;
     }
 }
